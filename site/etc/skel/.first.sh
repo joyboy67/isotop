5d52dec754166ccd8a48669ecea7ae1d1aec6c7f
Thuban <thuban@yeuxdelibad.net>
2017-10-15 10:58:02 +0200

    various improvements to use xfmpc

---
+++ site/etc/skel/.first.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# first session launch configuration
+
+# add xdg-user-dirs
+xdg-user-dirs-update
+
+# change mpd configuration
+sed -i -e "s;CHANGEME;$(xdg-user-dir MUSIC);" ~/.mpdconf
+
+rm $0
+exit 0
+
