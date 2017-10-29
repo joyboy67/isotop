0d1c7183038c8622a3efc1c86840c73c0c934661
Thuban <thuban@yeuxdelibad.net>
2017-10-29 10:11:35 +0100

    update translation to defautl en_EN

--- site/etc/skel/.first.sh
+++ site/etc/skel/.first.sh
@@ -22,7 +22,12 @@ case $lang in
 		echo 'export LC_CTYPE LC_MESSAGES LC_ALL LANG' >> ~/.profile
 	;;
 	*)
-		echo "No translation to do"
+		echo "No translation to do, default to en_EN"
+		echo 'LC_CTYPE="en_EN.UTF-8"' >> ~/.profile
+		echo 'LC_MESSAGES="en_EN.UTF-8"' >> ~/.profile
+		echo 'LC_ALL="en_EN.UTF-8"' >> ~/.profile
+		echo 'LANG="en_EN.UTF-8"' >> ~/.profile
+		echo 'export LC_CTYPE LC_MESSAGES LC_ALL LANG' >> ~/.profile
 	;;
 esac
 
