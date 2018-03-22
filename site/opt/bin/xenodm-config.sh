908c877499a13202eb766516a1dd428699413848
Thuban <thuban@yeuxdelibad.net>
2018-03-22 08:12:17 +0100

    gdoas and desktop files

--- site/opt/bin/xenodm-config.sh
+++ site/opt/bin/xenodm-config.sh
@@ -8,6 +8,7 @@
 WIDTH=400
 HEIGHT=300
 DEFAULTBG=/opt/isotop/walls/loginbg.jpg
+
 TITLE="Xenodm configuration"
 QUESTION="What do you wan't to configure?"
 TXT_CHGBG="Choose background image"
@@ -17,7 +18,22 @@ TXT_DISABLEAUTOLOGIN="Disable autologin"
 TXT_AUTOLOGIN_WARN="Enter username to automatically log in.
 Warning, this leaves your machine open to the world"
 
-RES=$(zeniTK --width $WIDTH --height $HEIGHT --text "$QUESTION" --list "$TXT_CHGBG" "$TXT_RDMBG" "$TXT_AUTOLOGIN")
+
+case $LANG in
+	fr*)
+		TITLE="Configuration de Xenodm"
+		QUESTION="Que souhaitez-vous configurer ?"
+		TXT_CHGBG="Choisir l'image d'arrière-plan"
+		TXT_RDMBG="Choisir un répertoire contenant les arrières-plans choisis au hasard"
+		TXT_AUTOLOGIN="Activer la connexion automatique"
+		TXT_DISABLEAUTOLOGIN="Désactiver la connexion automatique"
+		TXT_AUTOLOGIN_WARN="Entrez le nom de l'utilisateur à connecter automatiquement.
+Attention, cela laisse votre ordinateur ouvert pour tous."
+        ;;
+esac
+
+
+RES=$(zeniTK --width $WIDTH --height $HEIGHT --text "$QUESTION" --list "$TXT_CHGBG" "$TXT_RDMBG" "$TXT_AUTOLOGIN" "$TXT_DISABLEAUTOLOGIN")
 
 case "$RES" in
 	"$TXT_CHGBG") 
